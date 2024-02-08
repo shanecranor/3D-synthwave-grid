5727aa92d18a5763a1c3e80d7b463c973cae1e97
shanecranor <shanecranor@gmail.com>
2024-02-08 13:25:11 -0600

    fix perf issues, lineheight, and add bio

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,14 @@ import * as THREE from "three";
 import { BlendFunction } from "postprocessing";
 import "./App.scss";
 import { SynthwaveGrid } from "./components/3D/SynthwaveGrid";
-import { Cloud, Clouds, GradientTexture, Stars } from "@react-three/drei";
+import {
+  Cloud,
+  Clouds,
+  GradientTexture,
+  Stars,
+  Stats,
+  PerformanceMonitor,
+} from "@react-three/drei";
 import { Background } from "./components/3D/Background";
 class GlowMaterial extends THREE.MeshBasicMaterial {
   constructor() {
@@ -29,6 +36,7 @@ function App() {
     height: window.innerHeight,
     ratio: window.innerWidth / window.innerHeight,
   });
+  const [tile, setTile] = useState(0);
   useEffect(() => {
     const handleResize = () => {
       setWindowDimensions({
@@ -44,15 +52,62 @@ function App() {
   return (
     <>
       <main>
-        <h1>Shane Cranor</h1>
-        <nav>
-          <a>Code</a>
-          <a>Music</a>
-          <a>Photos</a>
-        </nav>
+        <div
+          className="nav-grid"
+          style={{ transform: `translateX(${tile * -100}vw)` }}
+        >
+          <div className="landing-page">
+            <h1
+              onClick={() => {
+                setTile((old) => (old + 1) % 2);
+              }}
+            >
+              Shane Cranor
+            </h1>
+            <nav>
+              <a>Code</a>
+              <a>Music</a>
+              <a>Photos</a>
+            </nav>
+          </div>
+          <section className="about-me">
+            <div className="page-contents">
+              <h2>About Me</h2>
+              <p>
+                Hey there! I am a full stack developer with Typescript and React
+                being my preferred languages. I recently graduated with a BS and
+                MS in Computer Science from the Colorado School of Mines. I play
+                bass, drums, guitar, and keys and I mixed and mastered an album
+                for my band, The Electric Army{" "}
+                <a href="https://open.spotify.com/artist/39DDKLQfC72gb8Q8cy4RIr">
+                  (listen here!)
+                </a>
+                .
+              </p>
+              <p>
+                Some fun facts about me: I'm a shill for Cloudflare
+                workers/pages, I love to cook, mess with electronics and make
+                music. I played collegiate Valorant (e-sports) in college and I
+                enjoy gaming occasionally.
+              </p>
+              <button
+                onClick={() => {
+                  setTile(tile - 1);
+                }}
+              >
+                back
+              </button>
+            </div>
+          </section>
+        </div>
       </main>
       <div className="three-js-canvas">
-        <Canvas>
+        <Canvas dpr={1}>
+          {/* <PerformanceMonitor
+            onIncline={() => setDpr(2)}
+            onDecline={() => setDpr(0.5)}
+          /> */}
+          <Stats />
           <SetCameraPosition />
           <Background windowDimensions={windowDimensions} />
           <Stars
@@ -132,7 +187,7 @@ function App() {
               speed={0.1}
               opacity={0.00001}
             />
-            {/* adds texture to the sky */}
+
             <Cloud
               seed={169}
               segments={60}
