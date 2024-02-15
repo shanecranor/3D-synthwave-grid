ad29033f2969b482c04c7c1fa0fee15f5b178bc6
shanecranor <shanecranor@gmail.com>
2024-02-15 00:53:08 -0600

    bugfixes for safari and added links

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -3,20 +3,27 @@ import { PlaceholderImage } from "@/components/PlaceholderImage/PlaceholderImage
 import ElectricArmy from "@/public/assets/img/electric-army.jpg";
 import "./page.scss";
 import Navbar from "@/components/old-components/Navbar/Navbar";
+import Link from "next/link";
 const Page = () => {
   return (
-    <main>
+    <main className="p-music">
       <Navbar />
       <h1>Music</h1>
       <div className="grid">
-        <section>
-          <h2>The Electric Army</h2>
-          <img src={ElectricArmy.src} alt="Hotbed of Descent Album Cover" />
-        </section>
-        <section>
-          <h2>Soundcloud</h2>
-          <PlaceholderImage seed="3D Arcade Game" animate />
-        </section>
+        <Link href="https://open.spotify.com/album/0wD82OWewTSDZiNVjRIaPT">
+          <section>
+            <h2>The Electric Army</h2>
+            <img src={ElectricArmy.src} alt="Hotbed of Descent Album Cover" />
+          </section>
+        </Link>
+        <Link href="https://soundcloud.com/shane_cranor">
+          <section>
+            <h2>Soundcloud</h2>
+            <div className="placeholder">
+              <PlaceholderImage seed="3D Arcade Game" animate />
+            </div>
+          </section>
+        </Link>
       </div>
     </main>
   );
--- src/components/old-components/LanguagesAndProjects/LanguagesAndProjects.tsx
+++ src/components/old-components/LanguagesAndProjects/LanguagesAndProjects.tsx
@@ -1,48 +1,49 @@
 "use client";
 import "./LanguagesAndProjects.scss";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import {
   getProjectsByLanguage,
   getProjectsByTag,
   projects,
 } from "@/data/projects";
-import React from "react";
+import React, { useState } from "react";
 import Project from "@/components/Project/Project";
-import { languages } from "@/data/languages";
 // import Router, { useRouter } from "next/router";
 export default function LanguagesAndProjects() {
-  // const { query } = useRouter();
-  const [selected, setSelected] = [
-    "All", //query.category
-    (category: string) => console.log("woo"),
-    // Router.push({ query: { ...query, category: category } }, undefined, {
-    //   shallow: true,
-    // }),
-  ];
-  const selectedProjects = getProjectsByTag(selected);
+  const [selectedProjects, setSelectedProjects] = useState(projects);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const languages = Array.from(
+    new Set(projects.map((project) => project.languages).flat())
+  );
+  const categories = Array.from(
+    new Set(projects.map((project) => project.tags).flat())
+  );
   return (
     <section className="all-projects">
       <section className="language-section">
         {/* <h1>Programming Languages & Libraries</h1> */}
         <div className="category-buttons">
-          {/* {categories.map((c) => (
+          {categories.map((c) => (
             <button
               key={c}
-              className={selected === c ? "selected" : "not-selected"}
-              onClick={() => setSelected(c)}
+              // className={selected === c ? "selected" : "not-selected"}
+              className="not-selected"
+              // onClick={() => setSelected(c)}
             >
               {c}
             </button>
-          ))} */}
+          ))}
         </div>
-        <div className="category-container">
-          {/* <div className='category-name'> {c}: </div> */}
+        {/* <div className="category-container">
           {languages.map((langObj) => (
             <span key={langObj.language} className="language">
               <img src={langObj.img} alt="" />
               {langObj.language}
             </span>
           ))}
-        </div>
+        </div> */}
       </section>
       <section className="selected-projects">
         {selectedProjects.map((project, index) => (
