bab2862369d4b0c9deb1753c7440dda1d1b91b17
shanecranor <shanecranor@gmail.com>
2024-02-15 09:01:11 -0600

    fix stupid build issue

--- src/components/old-components/LanguagesAndProjects/LanguagesAndProjects.tsx
+++ src/components/old-components/LanguagesAndProjects/LanguagesAndProjects.tsx
@@ -7,11 +7,56 @@ import {
   projects,
   tags,
 } from "@/data/projects";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { Suspense, useCallback, useEffect, useState } from "react";
 import Project from "@/components/Project/Project";
 import Link from "next/link";
 // import Router, { useRouter } from "next/router";
 export default function LanguagesAndProjects() {
+  return (
+    <>
+      <section className="all-projects">
+        <div
+          id="scroll-here"
+          style={{
+            position: "absolute",
+            marginTop: "900px",
+          }}
+        />
+        <Suspense
+          fallback={
+            <>
+              <div className="language-section">
+                <div className="category-buttons">
+                  {tags.map((c) => (
+                    <button key={c} className={"not-selected"}>
+                      {c}
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <section className="selected-projects">
+                  {projects.map((project, index) => (
+                    <Project
+                      key={project.title}
+                      data={project}
+                      onClick={() => {}}
+                    />
+                  ))}
+                </section>
+              </div>
+            </>
+          }
+        >
+          <CategorySelection />
+          <Projects />
+        </Suspense>
+      </section>
+    </>
+  );
+}
+
+const CategorySelection = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
@@ -24,73 +69,65 @@ export default function LanguagesAndProjects() {
     },
     [searchParams]
   );
-  const params = searchParams.get("category");
-  useEffect(() => {
-    if (params) {
-      const element = document.getElementById("scroll-here");
-      element &&
-        element.scrollIntoView({ behavior: "instant", block: "nearest" });
-    }
-  });
-  const selected = params || "All";
+  const selected = searchParams.get("category") || "All";
+  const categories = tags;
+  return (
+    <div className="language-section">
+      <div className="category-buttons">
+        {categories.map((c) => (
+          <button
+            key={c}
+            className={selected === c ? "selected" : "not-selected"}
+            onClick={() =>
+              router.replace(
+                pathname + "?" + createQueryString("category", c),
+                { scroll: false }
+              )
+            }
+          >
+            {c}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Projects = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const categoryParam = searchParams.get("category");
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+  const selected = categoryParam || "All";
   const projectsFiltered = projects.filter((project) => {
     if (selected === "All") return true;
     return project.tags.includes(selected);
   });
-  console.log(selected);
-  const categories = tags;
   return (
-    <>
-      <section className="all-projects">
-        <div
-          id="scroll-here"
-          style={{
-            position: "absolute",
-            marginTop: "900px",
-          }}
-        />
-        <section className="language-section">
-          {/* <h1>Programming Languages & Libraries</h1> */}
-          <div className="category-buttons">
-            {categories.map((c) => (
-              <button
-                key={c}
-                className={selected === c ? "selected" : "not-selected"}
-                onClick={() =>
-                  router.replace(
-                    pathname + "?" + createQueryString("category", c),
-                    { scroll: false }
-                  )
-                }
-              >
-                {c}
-              </button>
-            ))}
-          </div>
-          {/* <div className="category-container">
-          {languages.map((langObj) => (
-            <span key={langObj.language} className="language">
-              <img src={langObj.img} alt="" />
-              {langObj.language}
-            </span>
-          ))}
-        </div> */}
-        </section>
-        <section className="selected-projects">
-          {projectsFiltered.map((project, index) => (
-            <Project
-              key={project.title}
-              data={project}
-              onClick={() =>
-                router.replace(
-                  pathname + "?" + createQueryString("project", project.title),
-                  { scroll: false }
-                )
-              }
-            />
-          ))}
-        </section>
+    <div>
+      <section className="selected-projects">
+        {projectsFiltered.map((project, index) => (
+          <Project
+            key={project.title}
+            data={project}
+            onClick={() =>
+              router.replace(
+                pathname + "?" + createQueryString("project", project.title),
+                { scroll: false }
+              )
+            }
+          />
+        ))}
       </section>
-    </>
+    </div>
   );
-}
+};
